a87681b23649176320b325618d84eb0ba9d0e869
Matthew Jaoudi <gadfly361@gmail.com>
2016-06-28 09:06:23 -0700

    update core ns definitions;

--- src/cljs/soda_ash/macros.clj
+++ src/cljs/soda_ash/macros.clj
@@ -2,6 +2,7 @@
 
 
 (defmacro defcomp
+  "For use in implementation namepsaces"
   [symbol-name options]
   `(do
      (def ~(if (= "default" symbol-name)
@@ -12,3 +13,23 @@
              (symbol "component")
              (symbol (str "component-" symbol-name)))
        (t/create-component ~options))))
+
+
+(defmacro defc
+  "For use in core namepsace"
+  [comp-name type]
+  `(do
+     (def ~(if type
+             (symbol (str comp-name "-" type "'"))
+             (symbol (str comp-name "'")))
+       ~(if type
+          (symbol (str comp-name "/class-" type))
+          (symbol (str comp-name "/class"))))
+
+     (def ~(if type
+             (symbol (str comp-name "-" type))
+             (symbol (str comp-name)))
+       ~(if type
+          (symbol (str comp-name "/component-" type))
+          (symbol (str comp-name "/component"))))
+     ))
